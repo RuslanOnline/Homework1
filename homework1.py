82a3a92d3d9477fc8253ad82de5079dbd6cde3fa
RuslanOnline <scaineton@gmail.com>
2024-06-29 18:28:26 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,13 @@
+example="Sunflower"
+print("Sunflower"[0])
+print("Sunflower"[-1])
+print("Sunflower"[-5:])
+print("Sunflower"[::-1])
+print("Sunflower"[::2])
+    #Next option
+name="Sunflower"
+print(name[0])
+print(name[-1])
+print(name[-5:])
+print(name[::-1])
+print(name[::2])
